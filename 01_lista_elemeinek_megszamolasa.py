7cfe8eb1ba17b15c1369188da0c2832390eb98f0
Szalkay Erik <szalkay.erik@diak.szbi-pg.hu>
2024-11-20 13:40:19 +0100

    Add 01 and 02 task solution

--- 01_lista_elemeinek_megszamolasa.py
+++ 01_lista_elemeinek_megszamolasa.py
@@ -4,3 +4,15 @@ Határozd meg a lista hosszát egy ciklus segítségével
 (a len() függvény megoldásán kívül használj for ciklusos megoldást is),
 és írd ki!
 """
+
+list = ['5', '8', '12', '15', '22']
+
+print(len(list))
+
+hossz = 1
+for elem in list:
+    hossz = hossz + 1
+
+print(f"Lista elemei len methódussal: {len(list)}")
+print(f"Lista elemei for methódussal: {hossz}")
+    
